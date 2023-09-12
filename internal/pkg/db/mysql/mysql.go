c0475e83bde42e60eb53e839bf52256ab7d58cf4
kupuma-ru21 <kupumaru21@gmail.com>
2023-09-12 22:55:32 +0900

    feat: change db name

--- internal/pkg/db/mysql/mysql.go
+++ internal/pkg/db/mysql/mysql.go
@@ -13,8 +13,7 @@ import (
 var Db *sql.DB
 
 func InitDB() {
-	// Use root:dbpass@tcp(172.17.0.2)/hackernews, if you're using Windows.
-	db, err := sql.Open("mysql", "root:dbpass@tcp(localhost)/hackernews")
+	db, err := sql.Open("mysql", "root:dbpass@tcp(localhost)/review_pull_request")
 	if err != nil {
 		log.Panic(err)
 	}
